6e2f4818cc044b021863271caacde23b8fd7e714
Lu Xuan <myfather103@163.com>
2019-08-05 20:32:58 +0800

    Create build_openssl.sh

---
+++ build_openssl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function build_one
+{
+
+PWD='pwd'
+
+cd openssl-1.1.1c
+
+./config shared no-ssl2 no-ssl3 no-comp no-hw no-engine --openssldir=$PWD/../mac/openssl --prefix=$PWD/../mac/openssl
+
+make clean
+make depend
+make CALC_VERSIONS="SHLIB_COMPAT=; SHLIB_SOVER=" MAKE="make -e" all
+echo "place-holder make target for avoiding symlinks" >> $PWD/../linux/openssl/lib/link-shared
+make SHLIB_EXT=.so install_sw
+
+}
+
+build_one
